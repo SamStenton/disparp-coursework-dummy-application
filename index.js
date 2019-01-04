a33716e809cf794e17dd024e5a2e3436f6ddcdc8
Samuel Stenton <sthstenton@gmail.com>
2019-01-04 18:10:11 +0000

    force url

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(port, async () => {
   console.log(`Example app listening on port ${port}!`);
   // Make request to logging server
-  axios.get(process.env.GET_URL)
+  axios.get('https://google.co.uk')
     .then(() => console.log('Request to logging server made', Date.now()))
     .catch(console.log);
 });
